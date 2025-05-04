d991288386df640e378cc78bc84ac0eadd4e9379
Rafael Correa <rafaelscorreadev@gmail.com>
2025-05-04 15:42:26 -0300

    fix: corrige importação do tipo MenuItem

--- src/services/api.slice.ts
+++ src/services/api.slice.ts
@@ -1,5 +1,5 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
-import { MenuItem } from '../store/reducers/cart'
+import { MenuItem } from '../types/restaurant'
 
 export interface Restaurant {
   id: number
